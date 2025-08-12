909523d2ac23fed0dec2f8cc2e71efc8fbe0c225
Viaduct Maintainers <viaduct.maintainers@airbnb.com>
2025-08-12 05:25:46 +0000

    [viaduct][testing] Mv engine tests (#921809)
    Github-Change-Id: 921809
    Github-Change-Url: https://git.musta.ch/airbnb/treehouse/pull/921809
    GitOrigin-RevId: d15d567a17db1baabaea4e8bec60332b6a10cd42

---
+++ engine/runtime/src/test/kotlin/viaduct/engine/runtime/tenantloading/CycleDetectorFeatureTest.kt
@@ -0,0 +1,40 @@
+package viaduct.engine.runtime.tenantloading
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.Assertions.assertInstanceOf
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import viaduct.engine.api.GraphQLBuildError
+import viaduct.engine.api.mocks.MockTenantModuleBootstrapper
+import viaduct.engine.api.mocks.fetchAs
+import viaduct.engine.api.mocks.runFeatureTest
+
+@ExperimentalCoroutinesApi
+class CycleDetectorFeatureTest {
+    @Test
+    fun `cycles in resolver are detected`() {
+        // This should throw an exception during construction due to the cycle:
+        // foo requires bar, bar requires foo
+        val exception = assertThrows<GraphQLBuildError> {
+            MockTenantModuleBootstrapper("type Query { foo: Int, bar: Int }") {
+                field("Query" to "foo") {
+                    resolver {
+                        objectSelections("bar") // foo requires bar
+                        fn { _, obj, _, _, _ -> obj.fetchAs<Int>("bar") }
+                    }
+                }
+                field("Query" to "bar") {
+                    resolver {
+                        objectSelections("foo") // bar requires foo - creates cycle
+                        fn { _, obj, _, _, _ -> obj.fetchAs<Int>("foo") }
+                    }
+                }
+            }.runFeatureTest { } // Empty block - just triggers construction
+        }
+
+        // Look for RequiredSelectionsCycleException in the cause chain
+        val cause = generateSequence(exception.cause) { it.cause }
+            .firstOrNull { it is RequiredSelectionsCycleException }
+        assertInstanceOf(RequiredSelectionsCycleException::class.java, cause)
+    }
+}
