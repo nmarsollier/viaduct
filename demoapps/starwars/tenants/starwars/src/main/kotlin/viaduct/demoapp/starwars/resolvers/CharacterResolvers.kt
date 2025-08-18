5a99554da5d556d7b66199e46749a3568a89e86c
Viaduct Maintainers <viaduct.maintainers@airbnb.com>
2025-08-18 17:16:22 +0000

    [Viaduct][DemoApp] Adds usage of Batch Resolvers (#924174)
    Github-Change-Id: 924174
    Github-Change-Url: https://git.musta.ch/airbnb/treehouse/pull/924174
    GitOrigin-RevId: 04482f59d3e8137feb1551cc8faf2b7fbf0f99ef

--- demoapps/starwars/tenants/starwars/src/main/kotlin/viaduct/demoapp/starwars/resolvers/CharacterResolvers.kt
+++ demoapps/starwars/tenants/starwars/src/main/kotlin/viaduct/demoapp/starwars/resolvers/CharacterResolvers.kt
@@ -4,36 +4,6 @@ import viaduct.api.Resolver
 import viaduct.demoapp.starwars.resolverbases.CharacterResolvers
 
 /**
- * Character resolvers - only for complex/computed fields that have @resolver directives.
- * Simple scalar properties (name, birthYear, eyeColor, gender, hairColor, height, mass,
- * created, edited) are handled directly by the framework without custom resolvers.
- */
-
-/**
- * @resolver directive: Custom field resolution for Character.homeworld
- * Performs a lookup to find the planet associated with this character
- */
-@Resolver
-class CharacterHomeworldResolver : CharacterResolvers.Homeworld() {
-    override suspend fun resolve(ctx: Context): viaduct.api.grts.Planet? {
-        // TODO: Implement homeworld resolution without globalIDFor
-        return null
-    }
-}
-
-/**
- * @resolver directive: Custom field resolution for Character.species
- * Performs a lookup to find the species associated with this character
- */
-@Resolver
-class CharacterSpeciesResolver : CharacterResolvers.Species() {
-    override suspend fun resolve(ctx: Context): viaduct.api.grts.Species? {
-        // TODO: Implement species resolution without globalIDFor
-        return null
-    }
-}
-
-/**
  * Demonstrates shorthand fragment syntax - delegates to the name field
  * @resolver("name"): Shorthand fragment syntax that delegates resolution to another field.
  *                   This resolver will automatically fetch the "name" field and return its value.
