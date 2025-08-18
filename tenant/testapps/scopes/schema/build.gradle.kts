650560c11f77dc840b474563200773c222c1c437
Viaduct Maintainers <viaduct.maintainers@airbnb.com>
2025-08-18 09:54:14 +0000

    [Viaduct][OSS] Removes duplicate scopes test on testapps/s... (#924713)
    Github-Change-Id: 924713
    Github-Change-Url: https://git.musta.ch/airbnb/treehouse/pull/924713
    GitOrigin-RevId: 69ac75e18ae56baa27d01104e2a4179552d7db70

--- tenant/testapps/scopes/schema/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-/**
- * Don't add anything here, this is for viaduct.
- */
-
-// Please don't add anything
-
-// random spaces in between
-
-/** testing we can safely identify and ignore comments from Gradle config                   */
