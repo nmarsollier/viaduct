1c928a0a35052bc786bde3387040bbe1078ab317
Rusty Phillips <walter.phillips@ext.airbnb.com>
2025-08-25 14:47:34 -0400

    Changes build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,83 @@ group = groupId
 
 val jarVersion = projectVersion.get()
 
+val ktlintCliVersion = libs.versions.ktlintVersion
+val junitVersion = libs.versions.junit5
+val junitLib = libs.junit
+val kotlinTestLib = libs.kotlin.test
+
+repositories {
+    mavenCentral()
+}
+
+plugins.withId("org.jetbrains.kotlin.jvm") {
+    configure<KotlinJvmProjectExtension> {
+        jvmToolchain(21)
+    }
+}
+
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    ignoreFailures.set(true)
+}
+
+configure<com.autonomousapps.DependencyAnalysisExtension> {
+    issues {
+        all {
+            onUnusedDependencies {
+                severity("fail")
+            }
+            onUsedTransitiveDependencies {
+                severity("warn")
+            }
+            onIncorrectConfiguration {
+                severity("warn")
+            }
+        }
+    }
+
+    dependencies {
+        bundle("kotlin") {
+            includeGroup("org.jetbrains.kotlin")
+        }
+    }
+}
 subprojects {
     group = groupId
     version = jarVersion
+
+    plugins.apply("org.jetbrains.kotlin.jvm")
+    plugins.apply("io.gitlab.arturbosch.detekt")
+    plugins.apply("org.jlleitschuh.gradle.ktlint")
+
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        configure<KotlinJvmProjectExtension> {
+            jvmToolchain(21)
+        }
+
+        dependencies {
+            add("testImplementation", junitLib)
+            add("testImplementation", kotlinTestLib)
+            add("testImplementation", rootProject.libs.konsist)
+            add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher")
+        }
+    }
+
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
+
+    detekt {
+        source.setFrom("src/main/kotlin", "src/test/kotlin", "src/testFixtures/kotlin")
+        config.setFrom(file("$rootDir/detekt.yml"))
+        ignoreFailures = true
+    }
+
+    ktlint {
+        version.set(ktlintCliVersion) // Specify the ktlint version
+        enableExperimentalRules.set(true) // Optional: Enable experimental rules
+        outputToConsole.set(true) // Optional: Output results to the console
+        ignoreFailures.set(true)
+    }
 }
 
 tasks.register("cleanBuildAndPublish") {
