36a9a926b05b8e19c4add4ad5e9090c0b54a0f16
Adam Miskiewicz <skevy@users.noreply.github.com>
2025-09-17 17:31:34 +0000

    publish 0.3.0
    Github-Change-Id: 937645
    GitOrigin-RevId: fa8f3c690ba8587b07f75a029b6762234f596671

--- demoapps/cli-starter/settings.gradle.kts
+++ demoapps/cli-starter/settings.gradle.kts
@@ -1,25 +1,10 @@
 rootProject.name = "viaduct-cli-starter"
 
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven {
-            name = "Central Portal Snapshots"
-            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-        }
-    }
-}
-
 val viaductVersion: String by settings
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven {
-            name = "Central Portal Snapshots"
-            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-        }
     }
     versionCatalogs {
         create("libs") {
