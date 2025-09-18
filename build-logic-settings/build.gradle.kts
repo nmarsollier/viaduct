cf9ec512c2083691a52c43d64447c6d83236cf8f
József Bartók <bjozsef@gmail.com>
2025-09-18 09:47:30 +0300

    Rename `gradle-enterprise` to `develocity`

--- build-logic-settings/build.gradle.kts
+++ build-logic-settings/build.gradle.kts
@@ -7,7 +7,7 @@ description = "Provides SETTINGS level convention plugins for the build"
 
 dependencies {
     // Our internal gradle enterprise deployment can't handle a higher version
-    implementation(plugin(libs.plugins.gradle.enterprise))
+    implementation(plugin(libs.plugins.develocity))
     implementation(plugin(libs.plugins.foojay.resolver.convention))
     implementation(plugin(libs.plugins.dokka))
 }
