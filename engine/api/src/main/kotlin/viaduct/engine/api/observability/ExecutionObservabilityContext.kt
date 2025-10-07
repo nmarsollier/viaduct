9d9e055af7ee89dd7a8266924c89f20cf11b6338
Vickey Yeh <vickeyyeh@users.noreply.github.com>
2025-10-07 16:36:59 +0000

    add resolved_by tag to field metrics
    Github-Change-Id: 941259
    GitOrigin-RevId: 17fdd683fedb074340dff6ffca75f817d6d98449

--- engine/api/src/main/kotlin/viaduct/engine/api/observability/ExecutionObservabilityContext.kt
+++ engine/api/src/main/kotlin/viaduct/engine/api/observability/ExecutionObservabilityContext.kt
@@ -1,13 +1,16 @@
 package viaduct.engine.api.observability
 
 import viaduct.engine.api.ExecutionAttribution
+import viaduct.engine.api.ResolverMetadata
 
 /**
  * Context for observability.
  * This context is used to provide information about the execution that can be used by instrumentations (for tracing or metrics).
  *
  * @property attribution The attribution of the current execution.
+ * @property resolverMetadata Metadata of the resolver that resolved the field
  */
 data class ExecutionObservabilityContext(
-    val attribution: ExecutionAttribution?
+    val attribution: ExecutionAttribution? = null,
+    val resolverMetadata: ResolverMetadata? = null
 )
