816c7fd56674b784bac2727669aa92c960fb2296
Amity Wang <amity177@users.noreply.github.com>
2025-10-09 20:36:52 +0000

    Introduce TemporaryBypassAccessCheck to ...
    Github-Change-Id: 946378
    GitOrigin-RevId: a8410dd8e64d71e00ab5cc82de4278a18184b54e

---
+++ engine/api/src/main/kotlin/viaduct/engine/api/TemporaryBypassAccessCheck.kt
@@ -0,0 +1,22 @@
+package viaduct.engine.api
+
+import graphql.language.Field
+
+@Deprecated("For Airbnb use only, will be removed.")
+interface TemporaryBypassAccessCheck {
+    fun shouldBypassCheck(
+        field: Field,
+        bypassChecksDuringCompletion: Boolean
+    ): Boolean
+
+    /** A default implementation that only respect the given flag to bypass all checks,
+     * but does not bypass individual check per field.
+     * `bypassChecksDuringCompletion` is set to false by default, only overriden by shims.
+     */
+    object Default : TemporaryBypassAccessCheck {
+        override fun shouldBypassCheck(
+            field: Field,
+            bypassChecksDuringCompletion: Boolean
+        ): Boolean = bypassChecksDuringCompletion
+    }
+}
