579f2059fb8e79ada8fc10676a3db5528caf5ef0
viaductbot <viaduct-maintainers@airbnb.com>
2025-10-11 00:47:49 -0600

    Pass dynamic schema into checker executors and providers. (#63)
    Github-Change-Id: 949208
    GitOrigin-RevId: 80169e13e15a71ed12467270d2bb7d9a75f87bdd
    
    Co-authored-by: Josie Kupke <josie-kupke@users.noreply.github.com>

--- engine/api/src/test/kotlin/viaduct/engine/api/NoOpCheckerExecutorFactoryImplTest.kt
+++ engine/api/src/test/kotlin/viaduct/engine/api/NoOpCheckerExecutorFactoryImplTest.kt
@@ -2,19 +2,21 @@ package viaduct.engine.api
 
 import kotlin.test.assertNull
 import org.junit.jupiter.api.Test
+import viaduct.engine.api.mocks.MockSchema
 
 internal class NoOpCheckerExecutorFactoryImplTest {
     private val testSubject = NoOpCheckerExecutorFactoryImpl()
+    private val mockSchema = MockSchema.mk("type AnyType { anyField: String }")
 
     @Test
     fun `Test no op functionality`() {
-        val provider = testSubject.checkerExecutorForField("anyType", "anyField")
+        val provider = testSubject.checkerExecutorForField(mockSchema, "anyType", "anyField")
         assertNull(provider)
     }
 
     @Test
     fun `Test no op on node`() {
-        val provider = testSubject.checkerExecutorForType("anyType")
+        val provider = testSubject.checkerExecutorForType(mockSchema, "anyType")
         assertNull(provider)
     }
 }
