583c901ed4bef5862a79e26236c1373e0b33a92c
Aileen Chen <gummybug@users.noreply.github.com>
2025-10-14 21:17:14 +0000

    Register all modern access checkers
    Github-Change-Id: 950305
    GitOrigin-RevId: 25d8a32567b380d23ff7a4f7d4bacd262cde05ad

--- engine/runtime/src/test/kotlin/viaduct/engine/runtime/tenantloading/CheckerSelectionSetsAreProperlyTypedTest.kt
+++ engine/runtime/src/test/kotlin/viaduct/engine/runtime/tenantloading/CheckerSelectionSetsAreProperlyTypedTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import viaduct.engine.api.Coordinate
 import viaduct.engine.api.RequiredSelectionSet
 import viaduct.engine.api.mocks.MockCheckerExecutor
 import viaduct.engine.api.mocks.MockSchema
@@ -13,52 +12,52 @@ import viaduct.engine.api.mocks.mkRSS
 class CheckerSelectionSetsAreProperlyTypedTest {
     @Test
     fun `valid -- no selection sets`() {
-        assertValid("User", "name", emptyMap())
+        assertValid("User", emptyMap())
     }
 
     @Test
     fun `valid -- single selection set with correct type`() {
         val rss = mkCheckerRSS("User", "id name")
-        assertValid("User", "name", mapOf("key1" to rss))
+        assertValid("User", mapOf("key1" to rss))
     }
 
     @Test
     fun `valid -- multiple selection sets with correct type`() {
         val rss1 = mkCheckerRSS("User", "id")
         val rss2 = mkCheckerRSS("User", "name email")
-        assertValid("User", "name", mapOf("key1" to rss1, "key2" to rss2))
+        assertValid("User", mapOf("key1" to rss1, "key2" to rss2))
     }
 
     @Test
     fun `valid -- selection sets with null values`() {
         val rss = mkCheckerRSS("User", "id")
-        assertValid("User", "name", mapOf("key1" to rss, "key2" to null))
+        assertValid("User", mapOf("key1" to rss, "key2" to null))
     }
 
     @Test
     fun `valid -- single selection set on rooot query`() {
         val rss = mkCheckerRSS("Query", "currentUser")
-        assertValid("User", "name", mapOf("key1" to rss))
+        assertValid("User", mapOf("key1" to rss))
     }
 
     @Test
     fun `valid -- multiple selection sets with object type and root query`() {
         val validRss = mkCheckerRSS("User", "id")
         val invalidRss = mkCheckerRSS("Query", "currentUser")
-        assertValid("User", "name", mapOf("valid" to validRss, "invalid" to invalidRss))
+        assertValid("User", mapOf("valid" to validRss, "invalid" to invalidRss))
     }
 
     @Test
     fun `invalid -- single selection set with wrong type`() {
         val rss = mkCheckerRSS("Post", "title") // Wrong! Should be on either "User" or "Query"
-        assertInvalid("User", "name", mapOf("key1" to rss), "Post")
+        assertInvalid("User", mapOf("key1" to rss), "Post")
     }
 
     @Test
     fun `invalid -- all selection sets with wrong types`() {
         val rss1 = mkCheckerRSS("Comment", "body") // Wrong! Should be on either "User" or "Query"
         val rss2 = mkCheckerRSS("Post", "title") // Wrong! Should be on either "User" or "Query"
-        assertInvalid("User", "name", mapOf("key1" to rss1, "key2" to rss2), "Comment", "Post")
+        assertInvalid("User", mapOf("key1" to rss1, "key2" to rss2), "Comment", "Post")
     }
 
     @Test
@@ -68,7 +67,6 @@ class CheckerSelectionSetsAreProperlyTypedTest {
         val invalidRss = mkCheckerRSS("Post", "title") // Wrong! Should on either "User" or "Query"
         assertInvalid(
             "User",
-            "name",
             mapOf("valid1" to validRss1, "valid2" to validRss2, "invalid" to invalidRss),
             "Post"
         )
@@ -81,7 +79,6 @@ class CheckerSelectionSetsAreProperlyTypedTest {
         val rss3 = mkCheckerRSS("Comment", "body") // Wrong! Should on either "User" or "Query"
         assertInvalid(
             "User",
-            "name",
             mapOf("key1" to rss1, "key2" to rss2, "key3" to rss3),
             "Post",
             "Comment"
@@ -93,11 +90,10 @@ class CheckerSelectionSetsAreProperlyTypedTest {
      */
     private fun assertValid(
         typeName: String,
-        fieldName: String,
         requiredSelectionSets: Map<String, RequiredSelectionSet?>
     ) {
         assertDoesNotThrow {
-            validateCheckerSelectionSets(typeName, fieldName, requiredSelectionSets)
+            validateCheckerSelectionSets(typeName, requiredSelectionSets)
         }
     }
 
@@ -107,12 +103,11 @@ class CheckerSelectionSetsAreProperlyTypedTest {
      */
     private fun assertInvalid(
         typeName: String,
-        fieldName: String,
         requiredSelectionSets: Map<String, RequiredSelectionSet?>,
         vararg expectedInvalidTypeNames: String
     ) {
         val exception = assertThrows<Exception> {
-            validateCheckerSelectionSets(typeName, fieldName, requiredSelectionSets)
+            validateCheckerSelectionSets(typeName, requiredSelectionSets)
         }
 
         for (invalidTypeName in expectedInvalidTypeNames) {
@@ -129,7 +124,6 @@ class CheckerSelectionSetsAreProperlyTypedTest {
      */
     private fun validateCheckerSelectionSets(
         typeName: String,
-        fieldName: String,
         requiredSelectionSets: Map<String, RequiredSelectionSet?>
     ) {
         val schema = MockSchema.mk(
@@ -157,8 +151,8 @@ class CheckerSelectionSetsAreProperlyTypedTest {
 
         val checker = MockCheckerExecutor(requiredSelectionSets = requiredSelectionSets)
 
-        val ctx = FieldCheckerExecutorValidationCtx(
-            coord = Coordinate(typeName, fieldName),
+        val ctx = CheckerExecutorValidationCtx(
+            typeName = typeName,
             executor = checker
         )
 
