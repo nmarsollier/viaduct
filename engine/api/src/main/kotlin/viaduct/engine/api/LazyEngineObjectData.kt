bb106d899db86d1bfe11fe307228e34f7c624d33
Aileen Chen <gummybug@users.noreply.github.com>
2025-10-16 19:14:40 +0000

    Fix bug with lazy EODs resolving twice If the client query includes a node, and a resolver RSS also includes the same node in the same OER (same path from the root query), we attempt to call `resolveData` on the lazyeod twice, which ends up calling the node resolver twice. This also results in OER.resolve() throwing since it's already been resolved. This fixes this bug, and also migrates NodeResolverTest from the old to the new engine feature test framework so I can add a test case there. Github-Change-Id: 951240
    GitOrigin-RevId: ac6e62c630c899e7468bfca9f8069252d9bbb12f

--- engine/api/src/main/kotlin/viaduct/engine/api/LazyEngineObjectData.kt
+++ engine/api/src/main/kotlin/viaduct/engine/api/LazyEngineObjectData.kt
@@ -4,8 +4,13 @@ package viaduct.engine.api
  * An EngineObjectData that is not fully resolved until [resolveData] is called.
  */
 interface LazyEngineObjectData : EngineObjectData {
+    /**
+     * Resolves the data for this lazy object.
+     *
+     * @return true if the data was resolved by this call, false if it was already called previously
+     */
     suspend fun resolveData(
         selections: RawSelectionSet,
         context: EngineExecutionContext
-    )
+    ): Boolean
 }
