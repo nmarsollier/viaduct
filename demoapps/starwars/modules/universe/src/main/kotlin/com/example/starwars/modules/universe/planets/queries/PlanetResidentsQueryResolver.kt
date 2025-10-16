eb00d02536f06c9456dd7437dac2ad71887bd08c
Nestor Marsollier <nmarsollier@users.noreply.github.com>
2025-10-16 18:23:44 +0000

    Repackage Starwars demoapp Repackage of the starwars modules
    New Package structure:
    
    ```
    com.example.starwars.modules  (as tenant base)
       filmography
           characters
           films
           starwars (temporary until we have subqueries)
       universe
           planets
           species
           starships
           vehicles
    
    Each package contains:
               models  (business definitions)
               resolvers  (viadcut resolvers)
    ```
    
    Main module package was changed to :
    `    com.example.starwars.service
    `
    Extra : remove *TenantModules implementations.
    Github-Change-Id: 950862
    GitOrigin-RevId: c475962268d31fdb406fdf2558d26103a15f1494

---
+++ demoapps/starwars/modules/universe/src/main/kotlin/com/example/starwars/modules/universe/planets/queries/PlanetResidentsQueryResolver.kt
@@ -0,0 +1,35 @@
+package com.example.starwars.modules.universe.planets.queries
+
+import com.example.starwars.modules.universe.planets.models.PlanetsResidentsRepository
+import com.example.starwars.universe.resolverbases.PlanetResolvers
+import viaduct.api.Resolver
+import viaduct.api.grts.Character
+
+/**
+ * Resolver to fetch the residents of a planet.
+ * It retrieves characters whose homeworld matches the planet's ID.
+ */
+@Resolver("id")
+class PlanetResidentsQueryResolver : PlanetResolvers.Residents() {
+    /**
+     * Resolves the list of residents for a given planet based on the context.
+     *
+     * @param ctx The context containing the planet information.
+     * @return A list of viaduct `Character` objects who are residents of the planet, or null if none are found.
+     */
+    override suspend fun resolve(ctx: Context): List<Character>? {
+        // Related Planet ID is stored in the ctx object value.
+        val planetId = ctx.objectValue.getId().internalID
+
+        // Fetch residents associated with the planet from the repository.
+        val residents = PlanetsResidentsRepository.findResidentsByPlanetId(planetId)
+
+        return residents.map {
+            // Create a global ID for the Character using its internal ID.
+            val globalID = ctx.globalIDFor(Character.Reflection, it.characterId)
+
+            // Request Viaduct to resolve the Character node using the global ID.
+            ctx.nodeFor(globalID)
+        }
+    }
+}
