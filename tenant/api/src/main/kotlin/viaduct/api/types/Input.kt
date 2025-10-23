ab301c7ea58e3b083a55dbcda277bb075968e894
Raymie Stata <rstata@users.noreply.github.com>
2025-10-23 11:43:43 +0000

    refactor: Further consolidation of GRT factories In transitioning from `context` to `context2` we continue to align the two _prior_ to the actual transition.  What we're discovering is that factories for GRT instances are a bit fractured.  And further, to support testing -- and especially testing with _out_ codegen (ie, "feature tests"), there are special-cases in the factories that, combined with the facturedness, impedes maintenance.
    To address this issue, this PR further consolidates GRT factories into GRTConstructorExtensions.kt, which we introduced a while ago to help align `context` and `context2`.  An important element of this PR is a more systematic approach to "fake" GRTs.  These are GRTs that are _not_ generated by codegen, and therefore do not conform to the naming and other invariants of "official" GRTs, but are very helpful for testing without codegen.  These used to live in a file called `stubs.kt` and required mocking and other mechanisms to properly get injected into tests.  By giving these fakes first-class status in our GRT factories, we decouple the implementation of contexts from these helpful abstractions.
    Github-Change-Id: 952709
    GitOrigin-RevId: d0152af70a00c4adcdc0de031c58c8bc73576583

--- tenant/api/src/main/kotlin/viaduct/api/types/Input.kt
+++ tenant/api/src/main/kotlin/viaduct/api/types/Input.kt
@@ -8,11 +8,22 @@ import viaduct.engine.api.ViaductSchema
  */
 interface Input : InputLike {
     companion object {
+        /**
+         * Returns input type [name] from [schema].
+         *
+         * @throws IllegalArgumentException if [name] doesn't exist or is
+         * not an input type.
+         */
         fun inputType(
             name: String,
             schema: ViaductSchema
         ): GraphQLInputObjectType {
-            return schema.schema.getTypeAs(name)
+            val result = requireNotNull(schema.schema.getType(name)) {
+                "Type $name does not exist in schema."
+            }
+            return requireNotNull(result as? GraphQLInputObjectType) {
+                "Type $name ($result) is not an input type."
+            }
         }
     }
 }
