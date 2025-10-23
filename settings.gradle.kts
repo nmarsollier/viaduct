95922cbb30d14f2f41116ccbe23325f491d205cb
Goncalo Almeida <Goncalo-Almeida@users.noreply.github.com>
2025-10-23 21:01:40 +0000

    Add ktor-starter demo app to Viaduct OSS
    Github-Change-Id: 953167
    GitOrigin-RevId: 5173eadc3d2e6aae4a97b8065ac0cf9c98fd5d1d

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,6 +25,7 @@ includeBuild("gradle-plugins") {
 
 // demo apps
 includeBuild("demoapps/cli-starter")
+includeBuild("demoapps/ktor-starter")
 includeBuild("demoapps/starwars")
 includeBuild("demoapps/spring-starter")
 
