2036acd103ed0fe007034517041eb8beb4aefb20
Geovanne Duarte <geovannefduarte@users.noreply.github.com>
2025-10-26 00:06:54 +0000

    add observability methods to ViaductBuilder
    Github-Change-Id: 955301
    GitOrigin-RevId: 70fc51a2748538c6c17915da28681d59e42ed85d

--- service/wiring/src/test/kotlin/viaduct/service/ViaductBuilderTest.kt
+++ service/wiring/src/test/kotlin/viaduct/service/ViaductBuilderTest.kt
@@ -1,13 +1,22 @@
 package viaduct.service
 
+import graphql.execution.DataFetcherExceptionHandler
+import graphql.execution.DataFetcherExceptionHandlerParameters
+import graphql.execution.DataFetcherExceptionHandlerResult
 import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.SchemaGenerator
 import graphql.schema.idl.SchemaParser
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import java.util.concurrent.CompletableFuture
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertSame
 import org.junit.jupiter.api.Test
 import viaduct.engine.api.ViaductSchema
 import viaduct.service.api.spi.Flag
 import viaduct.service.api.spi.FlagManager
+import viaduct.service.api.spi.ResolverErrorBuilder
+import viaduct.service.api.spi.ResolverErrorReporter
 import viaduct.service.runtime.SchemaConfiguration
 
 class ViaductBuilderTest {
@@ -49,6 +58,191 @@ class ViaductBuilderTest {
             .build().let {
                 assertNotNull(it)
             }
+    }
+
+    @Test
+    fun testWithMeterRegistry() {
+        val meterRegistry = SimpleMeterRegistry()
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withMeterRegistry(meterRegistry)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testWithResolverErrorReporter() {
+        val errorReporter = ResolverErrorReporter { exception, fieldDefinition, dataFetchingEnvironment, errorMessage, metadata ->
+            // No-op for testing
+        }
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withResolverErrorReporter(errorReporter)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testWithDataFetcherErrorBuilder() {
+        val errorBuilder = ResolverErrorBuilder.NoOpResolverErrorBuilder
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withDataFetcherErrorBuilder(errorBuilder)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testWithDataFetcherExceptionHandler() {
+        val exceptionHandler = object : DataFetcherExceptionHandler {
+            override fun handleException(handlerParameters: DataFetcherExceptionHandlerParameters): CompletableFuture<DataFetcherExceptionHandlerResult> {
+                return CompletableFuture.completedFuture(
+                    DataFetcherExceptionHandlerResult.newResult().build()
+                )
+            }
+        }
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withDataFetcherExceptionHandler(exceptionHandler)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testMethodChaining() {
+        val meterRegistry = SimpleMeterRegistry()
+        val errorReporter = ResolverErrorReporter.NoOpResolverErrorReporter
+        val errorBuilder = ResolverErrorBuilder.NoOpResolverErrorBuilder
+        val exceptionHandler = object : DataFetcherExceptionHandler {
+            override fun handleException(handlerParameters: DataFetcherExceptionHandlerParameters): CompletableFuture<DataFetcherExceptionHandlerResult> {
+                return CompletableFuture.completedFuture(
+                    DataFetcherExceptionHandlerResult.newResult().build()
+                )
+            }
+        }
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+
+        val builder = ViaductBuilder()
+        val result = builder
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withMeterRegistry(meterRegistry)
+            .withResolverErrorReporter(errorReporter)
+            .withDataFetcherErrorBuilder(errorBuilder)
+            .withDataFetcherExceptionHandler(exceptionHandler)
+            .standardNodeBehavior(false)
+
+        // Verify that method chaining returns the same builder instance
+        assertSame(builder, result)
+
+        val viaduct = result.build()
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testAllObservabilityMethodsTogether() {
+        val meterRegistry = SimpleMeterRegistry()
+        val errorReporter = ResolverErrorReporter.NoOpResolverErrorReporter
+        val errorBuilder = ResolverErrorBuilder.NoOpResolverErrorBuilder
+        val exceptionHandler = object : DataFetcherExceptionHandler {
+            override fun handleException(handlerParameters: DataFetcherExceptionHandlerParameters): CompletableFuture<DataFetcherExceptionHandlerResult> {
+                return CompletableFuture.completedFuture(
+                    DataFetcherExceptionHandlerResult.newResult().build()
+                )
+            }
+        }
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+
+        // Test that all observability methods can be used together
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withNoTenantAPIBootstrapper()
+            .withSchemaConfiguration(schemaConfiguration)
+            .withMeterRegistry(meterRegistry)
+            .withResolverErrorReporter(errorReporter)
+            .withDataFetcherErrorBuilder(errorBuilder)
+            .withDataFetcherExceptionHandler(exceptionHandler)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testObservabilityWithOtherBuilderMethods() {
+        val meterRegistry = SimpleMeterRegistry()
+        val errorReporter = ResolverErrorReporter.NoOpResolverErrorReporter
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+
+        // Test that observability methods work with other builder methods
+        val viaduct = ViaductBuilder()
+            .withFlagManager(flagManager)
+            .withMeterRegistry(meterRegistry) // Observability before other methods
+            .withNoTenantAPIBootstrapper()
+            .withResolverErrorReporter(errorReporter) // Observability in the middle
+            .withSchemaConfiguration(schemaConfiguration)
+            .standardNodeBehavior(false)
+            .build()
+
+        assertNotNull(viaduct)
+    }
+
+    @Test
+    fun testBuilderReturnsCorrectInstance() {
+        val meterRegistry = SimpleMeterRegistry()
+        val builder = ViaductBuilder()
+        val schemaConfiguration = SchemaConfiguration.fromSchema(
+            schema,
+            scopes = setOf(SchemaConfiguration.ScopeConfig("public", setOf("publicScope")))
+        )
+
+        val returned = builder.withMeterRegistry(meterRegistry)
+
+        // Verify that the method returns the same builder instance for chaining
+        assertSame(builder, returned)
+        assertEquals(builder, returned)
     }
 
     private fun mkSchema(sdl: String): ViaductSchema = ViaductSchema(SchemaGenerator().makeExecutableSchema(SchemaParser().parse(sdl), RuntimeWiring.MOCKED_WIRING))
