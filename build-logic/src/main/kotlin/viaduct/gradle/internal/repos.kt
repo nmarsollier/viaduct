6e2b3ff708310a981adf9fcab4161bc750b51570
Rusty Phillips <fireboy1919@users.noreply.github.com>
2025-10-28 15:17:56 +0000

    Remove CircleCI config
    Github-Change-Id: 956372
    GitOrigin-RevId: 521797e27ac1a1be9046f7b60e8fd082d4e9df6c

--- build-logic/src/main/kotlin/viaduct/gradle/internal/repos.kt
+++ build-logic/src/main/kotlin/viaduct/gradle/internal/repos.kt
@@ -7,7 +7,7 @@ import org.gradle.api.file.DirectoryProperty
 /**
  * Best-effort repo root detector. Looks for any of the [sentinels] at or above this project.
  */
-fun Project.repoRoot(vararg sentinels: String = arrayOf(".circleci", ".git")): DirectoryProperty {
+fun Project.repoRoot(vararg sentinels: String = arrayOf(".github", ".git")): DirectoryProperty {
     var cur: File? = rootDir
     while (cur != null) {
         if (sentinels.any { File(cur, it).exists() }) return objects.directoryProperty().fileValue(cur)
