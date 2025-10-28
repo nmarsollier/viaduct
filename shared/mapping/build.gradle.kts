ca84d733deece92d9799559ea2bfdd82d32a8962
James Bellenger <jbellenger@users.noreply.github.com>
2025-10-28 20:46:58 +0000

    JsonConv
    Github-Change-Id: 956352
    GitOrigin-RevId: 22a0b0f350572634a25ff848baca78d1dfff5c21

--- shared/mapping/build.gradle.kts
+++ shared/mapping/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     api(libs.viaduct.shared.invariants)
     api(libs.viaduct.shared.viaductschema)
 
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.databind)
     implementation(libs.viaduct.engine.api)
     implementation(libs.viaduct.shared.utils)
     implementation(libs.kotest.common.jvm)
@@ -16,9 +18,11 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core.jvm)
     implementation(libs.kotlinx.coroutines.test)
 
+    testImplementation(libs.graphql.java.extension)
     testImplementation(libs.kotest.assertions.shared)
     testImplementation(libs.kotest.property.jvm)
     testImplementation(libs.viaduct.shared.arbitrary)
+    testImplementation(testFixtures(libs.viaduct.engine.api))
 
     testFixturesImplementation(libs.kotlinx.coroutines.test)
     testFixturesImplementation(libs.kotest.property.jvm)
