d4a613ccf2e2cc88b70267c6ce62956b160d0d78
Aileen Chen <gummybug@users.noreply.github.com>
2025-10-30 20:01:38 +0000

    Mutation resolvers [1/n] - remove objectValue fr... Removes `objectValue` from `MutationFieldExecutionContext`, since we don't want to allow required selection sets on the root Mutation field. Mutation field resolvers can still execute other mutation fields using `ctx.mutation()`, but will not be able to do so via the `@Resolver` annotation. Github-Change-Id: 957794
    GitOrigin-RevId: ed923fc87cdd10657092e45d189ddd8b1d9471a4

---
+++ tenant/api/src/main/kotlin/viaduct/api/context/BaseFieldExecutionContext.kt
@@ -0,0 +1,35 @@
+package viaduct.api.context
+
+import viaduct.api.select.SelectionSet
+import viaduct.api.types.Arguments
+import viaduct.api.types.CompositeOutput
+import viaduct.api.types.Query
+
+/**
+ * Base [ExecutionContext] interface for mutation and non-mutation field resolvers
+ */
+interface BaseFieldExecutionContext<
+    Q : Query,
+    A : Arguments,
+    O : CompositeOutput
+> : ResolverExecutionContext {
+    /**
+     * A value of [Q], with any (and only) selections from [viaduct.api.Resolver.queryValueFragment]
+     * populated.
+     * Attempting to access fields not declared in [viaduct.api.Resolver.queryValueFragment] will
+     * throw a runtime exception
+     */
+    val queryValue: Q
+
+    /**
+     * The value of any [A] arguments that were provided by the caller of this
+     * resolver. If this field does not take arguments, this is [Arguments.NoArguments].
+     */
+    val arguments: A
+
+    /**
+     * The [SelectionSet] for [O] that the caller provided. If this field does not have a
+     * selection set (i.e. it has a scalar or enum type), this returns [SelectionSet.NoSelections].
+     */
+    fun selections(): SelectionSet<O>
+}
