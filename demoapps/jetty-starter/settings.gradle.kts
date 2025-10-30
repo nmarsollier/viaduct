7a64ad1e0cbf81f9208690d6743da8e7b68c5493
viaductbot <viaduct-maintainers@airbnb.com>
2025-10-29 21:36:55 -0400

    Add jetty-starter demoapp (#150)
    Github-Change-Id: 953830
    GitOrigin-RevId: fa3cb42c9d33f8ef94a55c37433cc677ab854291
    
    Co-authored-by: Goncalo Almeida <Goncalo-Almeida@users.noreply.github.com>

---
+++ demoapps/jetty-starter/settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "viaduct-jetty-starter"
+
+val viaductVersion: String by settings
+
+// When part of composite build, use local gradle-plugins
+// When standalone, use Maven Central (only after version is published)
+pluginManagement {
+    if (gradle.parent != null) {
+        includeBuild("../../gradle-plugins")
+    } else {
+        repositories {
+            mavenCentral()
+            gradlePluginPortal()
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    versionCatalogs {
+        create("libs") {
+            // This injects a dynamic value that your TOML can reference.
+            version("viaduct", viaductVersion)
+        }
+    }
+}
+
+include(":resolvers")
