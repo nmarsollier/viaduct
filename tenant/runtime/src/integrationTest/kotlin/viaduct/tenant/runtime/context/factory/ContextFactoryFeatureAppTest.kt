6d15f3ee8de22b2fd4992613624592280acb6716
Nestor Marsollier <nmarsollier@gmail.com>
2025-11-03 14:21:42 -0500

    Thread requestContext into the various Execution... (#22)
    Adds a feature to Viaduct whereby a `requestContext` value can be set as part of `ExecutionInput`, and that value will be available to tenant code via `ExecutionContext.requestContext`.  (Finishes work start ing #942226 which added `requestContext` to `ExecutionInput` - this PR gets that value into the tenant execution contexts.)
    Github-Change-Id: 945944
    
    GitOrigin-RevId: 0fc2591aa193338377f89c9d0ae6f1d31dce313b
    
    Co-authored-by: Raymie Stata <rstata@users.noreply.github.com>

--- tenant/runtime/src/integrationTest/kotlin/viaduct/tenant/runtime/context/factory/ContextFactoryFeatureAppTest.kt
+++ tenant/runtime/src/integrationTest/kotlin/viaduct/tenant/runtime/context/factory/ContextFactoryFeatureAppTest.kt
@@ -1,5 +1,13 @@
 package viaduct.tenant.runtime.context.factory
 
+import org.junit.jupiter.api.Test
+import viaduct.api.Resolver
+import viaduct.api.Variables
+import viaduct.api.VariablesProvider
+import viaduct.api.context.VariablesProviderContext
+import viaduct.api.types.Arguments
+import viaduct.graphql.test.assertEquals
+import viaduct.tenant.runtime.context.factory.resolverbases.QueryResolvers
 import viaduct.tenant.runtime.fixtures.FeatureAppTestBase
 
 class ContextFactoryFeatureAppTest : FeatureAppTestBase() {
@@ -7,7 +15,9 @@ class ContextFactoryFeatureAppTest : FeatureAppTestBase() {
         """
         #START_SCHEMA
         extend type Query {
-            empty: Int
+            valueFromArg(arg: Int): Int @resolver
+            reqCtxFromFieldCtx: Int @resolver
+            reqCtxFromVariableCtx: Int @resolver
         }
 
         type Foo {
@@ -22,7 +32,7 @@ class ContextFactoryFeatureAppTest : FeatureAppTestBase() {
           bars: [Bar]
         }
 
-        type Baz implements Node {
+        type Baz implements Node @resolver {
           id: ID!
           x: Int
         }
@@ -38,4 +48,64 @@ class ContextFactoryFeatureAppTest : FeatureAppTestBase() {
         }
         #END_SCHEMA
         """
+
+    val bazID = createGlobalIdString(Baz.Reflection, "ignore me")
+
+    @Resolver
+    class BazNodeResolver : NodeResolvers.Baz() {
+        override suspend fun resolve(ctx: Context): Baz = Baz.Builder(ctx).x(ctx.requestContext as Int).build()
+    }
+
+    @Resolver
+    class Query_ValueFromArg : QueryResolvers.ValueFromArg() {
+        override suspend fun resolve(ctx: Context) = ctx.arguments.arg
+    }
+
+    @Resolver
+    class Query_ReqCtxFromFieldCtx : QueryResolvers.ReqCtxFromFieldCtx() {
+        override suspend fun resolve(ctx: Context) = ctx.requestContext as Int
+    }
+
+    @Resolver(" valueFromArg(arg:\$var) ")
+    class Query_ReqCtxFromVariableCtx : QueryResolvers.ReqCtxFromVariableCtx() {
+        override suspend fun resolve(ctx: Context) = ctx.objectValue.getValueFromArg() as Int
+
+        @Variables("var: Int!")
+        class VarProvider : VariablesProvider<Arguments> {
+            override suspend fun provide(ctx: VariablesProviderContext<Arguments>) = mapOf("var" to ctx.requestContext)
+        }
+    }
+
+    @Test
+    fun `request context gets to field execution context`() {
+        execute("query { reqCtxFromFieldCtx }", requestContext = 42)
+            .assertEquals {
+                "data" to {
+                    "reqCtxFromFieldCtx" to 42
+                }
+            }
+    }
+
+    @Test
+    fun `request context gets to node execution context`() {
+        val bazID = createGlobalIdString(Baz.Reflection, "123")
+        execute("query { node(id: \"$bazID\") { ... on Baz { x } } }", requestContext = 42)
+            .assertEquals {
+                "data" to {
+                    "node" to {
+                        "x" to 42
+                    }
+                }
+            }
+    }
+
+    @Test
+    fun `request context gets to variable execution context`() {
+        execute("query { reqCtxFromVariableCtx }", requestContext = 42)
+            .assertEquals {
+                "data" to {
+                    "reqCtxFromVariableCtx" to 42
+                }
+            }
+    }
 }
