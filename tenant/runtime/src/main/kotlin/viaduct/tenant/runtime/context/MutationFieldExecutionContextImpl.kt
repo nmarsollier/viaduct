ede4bbbf1f5138b1dd1a1026f4a55bcabd5a357a
Nestor Marsollier <nmarsollier@gmail.com>
2025-11-03 14:42:52 -0500

    Decouple ResolverBase.Context from implementation (#68)
    The code generated for the nested `Context` class of resolver base classes was hard wired to specific implementations of `ExecutionContext`, which made it difficult to replace those implementations or inject alternatives for testing.  This PR switches the dependency to the `ExecutionContext` interfaces instead.
    
    Two other changes have been bundled:
    
    1. Eliminated the `XyzExecutionContextTmp` "transition" classes - the new `Context` implementation no longer needs them.
    
    2. Fixed the unit test for the type checking done by `DynamicValueBuilder`
    Github-Change-Id: 949529
    GitOrigin-RevId: 37a55cfdf1334d0ef4d4e1566884cca15465a7a4
    
    Co-authored-by: Raymie Stata <rstata@users.noreply.github.com>

--- tenant/runtime/src/main/kotlin/viaduct/tenant/runtime/context/MutationFieldExecutionContextImpl.kt
+++ tenant/runtime/src/main/kotlin/viaduct/tenant/runtime/context/MutationFieldExecutionContextImpl.kt
@@ -1,8 +1,8 @@
 package viaduct.tenant.runtime.context
 
 import viaduct.api.context.FieldExecutionContext
+import viaduct.api.context.MutationFieldExecutionContext
 import viaduct.api.internal.InternalContext
-import viaduct.api.internal.MutationFieldExecutionContextTmp
 import viaduct.api.internal.select.SelectionsLoader
 import viaduct.api.select.SelectionSet
 import viaduct.api.types.Arguments
@@ -14,6 +14,6 @@ import viaduct.api.types.Query
 class MutationFieldExecutionContextImpl<T : Object, Q : Query, A : Arguments, O : CompositeOutput>(
     private val inner: FieldExecutionContextImpl<T, Q, A, O>,
     private val mutationLoader: SelectionsLoader<Mutation>,
-) : FieldExecutionContext<T, Q, A, O> by inner, MutationFieldExecutionContextTmp<T, Q, A, O>, InternalContext by inner {
+) : FieldExecutionContext<T, Q, A, O> by inner, MutationFieldExecutionContext<T, Q, A, O>, InternalContext by inner {
     override suspend fun <T : Mutation> mutation(selections: SelectionSet<T>): T = mutationLoader.load(this, selections)
 }
