2e6cbd8b9d774a6921d6e79d0616ee9f3745ff77
Aileen Chen <gummybug@users.noreply.github.com>
2025-11-22 21:03:33 +0000

    Handle list item access check errors (AIRBNB)
    fix: Applies exception handling to list items during field completion in addition to where field values are being handled. This does both error reporting and converting exceptions into `GraphQLError`. Although the raw slot currently doesn't have exceptional values for list items, the access check slot may. This allows us to properly convert them instead of incorrectly wrapping them in `InternalEngineException`.
    
    Github-Change-Id: 967843
    GitOrigin-RevId: 701674aea2ac8e4f399cefcb02ac7e369fc47147

--- engine/runtime/src/main/kotlin/viaduct/engine/runtime/execution/FieldCompleter.kt
+++ engine/runtime/src/main/kotlin/viaduct/engine/runtime/execution/FieldCompleter.kt
@@ -104,7 +104,7 @@ class FieldCompleter(
                             Value.fromThrowable(err)
                         }
                 } else {
-                    objectFieldMap(parameters, parentOER).map { resolvedData ->
+                    objectFieldMap(parameters).map { resolvedData ->
                         ctxCompleteObject.onCompleted(resolvedData, null)
                         FieldCompletionResult.obj(resolvedData, parameters)
                     }
@@ -113,36 +113,24 @@ class FieldCompleter(
     }
 
     @Suppress("UNCHECKED_CAST")
-    private fun objectFieldMap(
-        parameters: ExecutionParameters,
-        parentOER: ObjectEngineResultImpl
-    ): Value<Map<String, Any?>> {
+    private fun objectFieldMap(parameters: ExecutionParameters): Value<Map<String, Any?>> {
+        val parentOER = parameters.parentEngineResult
         val fields = collectFields(parentOER.graphQLObjectType, parameters).selections
         val fieldValues = fields.map { field ->
             field as QueryPlan.CollectedField
 
             val newParams = parameters.forField(parentOER.graphQLObjectType, field)
             val fieldKey = buildOERKeyForField(newParams, field)
-            val dataFetchingEnvironmentProvider = { buildDataFetchingEnvironment(newParams, field, parentOER) }
-
             val bypassChecker = temporaryBypassAccessCheck.shouldBypassCheck(field.mergedField.singleField, parameters.bypassChecksDuringCompletion)
 
             // Obtain a result for this field
-            val combinedValue = combineValues(
+            val handledFieldValue = combineValues(
                 parentOER.getValue(fieldKey, RAW_VALUE_SLOT),
                 parentOER.getValue(fieldKey, ACCESS_CHECK_SLOT),
                 bypassChecker
-            )
+            ).handleException(newParams, field)
 
-            val handledFetch = combinedValue.recover { throwable ->
-                // Handle fetch errors gracefully
-                handleFetchingException(dataFetchingEnvironmentProvider, throwable)
-                    .map {
-                        FieldResolutionResult.fromErrors(it.errors)
-                    }
-            }
-
-            field.responseKey to completeField(field, newParams, handledFetch).map { it.value }
+            field.responseKey to completeField(field, newParams, handledFieldValue).map { it.value }
         }
 
         return Value.waitAll(fieldValues.map { it.second })
@@ -179,16 +167,37 @@ class FieldCompleter(
         val checkerResultValue = checkNotNull(checkerSlotValue as? Value<out CheckerResult?>) {
             "Expected checker slot to contain Value<out CheckerResult>, was ${checkerSlotValue.javaClass}"
         }
-        return fieldResolutionResultValue.flatMap {
-            // At this point the raw value resolved successfully
-            checkerResultValue.flatMap { checkerResult ->
-                checkerResult?.asError?.error?.let { Value.fromThrowable(it) } ?: fieldResolutionResultValue
+        return fieldResolutionResultValue.flatMap { frr ->
+            if (frr.errors.isNotEmpty()) {
+                fieldResolutionResultValue
+            } else {
+                // At this point the raw value resolved without errors, surface the checker error if it exists
+                checkerResultValue.flatMap { checkerResult ->
+                    checkerResult?.asError?.error?.let { Value.fromThrowable(it) } ?: fieldResolutionResultValue
+                }
             }
         }
     }
 
     /**
-     * Handles exceptions from data fetchers by delegating to the configured handler.
+     * If the this value is exceptional, converts it into a non-exceptional [FieldResolutionResult] value with
+     * handled errors.
+     */
+    private fun Value<FieldResolutionResult>.handleException(
+        params: ExecutionParameters,
+        field: QueryPlan.CollectedField,
+    ): Value<FieldResolutionResult> {
+        return this.recover { throwable ->
+            val dataFetchingEnvironmentProvider = { buildDataFetchingEnvironment(params, field, params.parentEngineResult) }
+            handleFetchingException(dataFetchingEnvironmentProvider, throwable)
+                .map {
+                    FieldResolutionResult.fromErrors(it.errors)
+                }
+        }
+    }
+
+    /**
+     * Handles exceptions from data fetchers and access checks by delegating to the configured handler.
      *
      * @param dataFetchingEnvironmentProvider The environment provider
      * @param exception The exception to handle
@@ -403,7 +412,8 @@ class FieldCompleter(
             val execStepInfoForItem =
                 executionStepInfoFactory.newExecutionStepInfoForListElement(parameters.executionStepInfo, indexedPath)
             val newParams = parameters.copy(executionStepInfo = execStepInfoForItem)
-            completeValue(field, newParams, item, null)
+            val handledItem = item.handleException(newParams, field)
+            completeValue(field, newParams, handledItem, null)
         }
 
         // Once all items are completed, transform them into a single FieldCompletionResult.
